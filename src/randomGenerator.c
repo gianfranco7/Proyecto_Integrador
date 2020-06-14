6fab77d99ea3506b9429714640313412731ef01f
Gianfranco <56212332+gianfranco7@users.noreply.github.com>
2020-06-14 17:28:55 -0600

    Update randomGenerator.c

--- src/randomGenerator.c
+++ src/randomGenerator.c
@@ -7,26 +7,4 @@ double random(int minimo, int maximo){
     return valor;
 }
 
-void leerFile(string ruta){
-    FILE *archivo;
-    char caracter;
 
-    archivo = fopen(ruta, "r");
-
-    if(archivo == NULL){
-        printf("Error a la hora de abrir el archivo");
-    }
-    else{
-        printf("\nEl contenido del archivo de prueba es \n\n");
-        while((caracter = fgets(archivo)) != EOF){
-            /* 
-            sensor()
-            hora()
-            min()
-            seg()
-            lectura()
-            */
-        }
-    }
-    fclose
-}
