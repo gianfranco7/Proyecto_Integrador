ed8d067bffa46c91e4a9dbe67a66a80f2fc5d918
Jose14valerio <56236035+Jose14valerio@users.noreply.github.com>
2020-06-14 17:02:22 -0600

    Update generadorArchivos.asm

--- src/generadorArchivos.asm
+++ src/generadorArchivos.asm
@@ -36,27 +36,32 @@ generadorArchivos:
     mov ebx, nom1    ;nombre del archivo
     mov eax, 8       ;SYS_CREAT es el system call 8
     int 80h          ;interrupcion del sistema
+    jmp _posArchivo
 	
     _sensorPulso:
     mov ebx, nom2    ;nombre del archivo
     mov eax, 8       ;SYS_CREAT es el system call 8
     int 80h          ;interrupcion del sistema
+    jmp _posArchivo
 
     _sensorPulso:
     mov ebx, nom3    ;nombre del archivo
     mov eax, 8       ;SYS_CREAT es el system call 8
     int 80h          ;interrupcion del sistema
+    jmp _posArchivo
 
     _sensorPulso:
     mov ebx, nom4    ;nombre del archivo
     mov eax, 8       ;SYS_CREAT es el system call 8
     int 80h          ;interrupcion del sistema
+    jmp _posArchivo
 
     _sensorPulso:
     mov ebx, nom5    ;nombre del archivo
     mov eax, 8       ;SYS_CREAT es el system call 8
     int 80h          ;interrupcion del sistema
 
+    _posArchivo:
     mov ebx, 0       ;codigo de salida
     mov eax, 1       ;SYS_EXIT es el system call 1
     int 80h          ;interrupcion del sistema
