cb9f98b83b0d7e4d9d97c317ca8ed0125f6b944e
Gianfranco <56212332+gianfranco7@users.noreply.github.com>
2020-06-14 16:47:09 -0600

    Update main.c

--- src/main.c
+++ src/main.c
@@ -9,7 +9,7 @@ extern void generarArchivo(int tipoArchivo):
 void main() {
     int tipoArchivo = 0;
     printf("Digite el tipo del archivo que desea generar ");
-    scanf("%lf", &tipoArchivo);
+    scanf("%i", &tipoArchivo);
     printf("El tipo de archivo digitado es: %i\n", tipoArchivo);
     generarArchivo(tipoArchivo);
     printf("Archivo generado exitosamente");
