82fd66889f45ab13e0827921f166ce9498cc545c
Gianfranco <56212332+gianfranco7@users.noreply.github.com>
2020-06-28 17:35:25 -0600

    Add files via upload

--- src/generadorRandoms.c
+++ src/generadorRandoms.c
@@ -1,15 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-double generadorRandoms(int minimo, int maximo){
-    srand((unsigned) time(&tm));
-    double valor = (rand() % ((maximo-minimo+1)+minimo)/10);
-    return valor;
-}
-
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <time.h> 
+double generadorRandoms(int minimo, int maximo){ 
+        int num = (rand() % (maximo - minimo + 1)) + minimo;
+        double rand = num/10;
+        return rand;	
+} 
+    
 int main(){
-        double prueba = generadorRandoms(90.4, 100.5);
-	printf("%f", prueba);
-	return 0;
+    int max, min;	
+    printf("Max");
+    scanf("%d", &max);
+    printf("Min");
+    scanf("%d", &min); 
+    srand(time(0));
+    printf("%lf ", generadorRandoms(min,max));
+    return 0;
 }
-
