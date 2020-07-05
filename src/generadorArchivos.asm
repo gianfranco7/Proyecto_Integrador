8bd560fc5e8ceed1783027d3e61cb0dc98319844
KevinGVR <39310978+KevinGVR@users.noreply.github.com>
2020-07-04 20:17:52 -0600

    Add files via upload

--- src/generadorArchivos.asm
+++ src/generadorArchivos.asm
@@ -1,11 +1,12 @@
 SECTION .data
 
 nom1 db './sensorPulso.txt', 00h
-nom2 db './sensorRitmo.txt', 00h
-nom3 db './sensorPresion.txt', 00h
+nom2 db './sensorTemperatura.txt', 00h
+nom3 db './sensorElectro.txt', 00h
 nom4 db './sensorTasaRespiratoria.txt', 00h
 nom5 db './sensorSaturacionOxigeno.txt', 00h
-valor dq 
+valor dq 'qwertyui'
+len equ $-valor
 
 SECTION .text
 
@@ -20,7 +21,7 @@ generadorArchivos:
     sub esp, 8
 
     ;crear archivo
-    mov edx, [ebp+8] ;parametro tipoArchivo
+    mov edx, 1;[ebp+8] ;parametro tipoArchivo
     mov ecx, 0o777   ;Permisos de lectura, escritura y ejecucion
     cmp edx, 1
     je _sensorPulso
@@ -36,43 +37,50 @@ generadorArchivos:
     _sensorPulso:
     mov ebx, nom1    ;nombre del archivo
     mov eax, 8       ;SYS_CREAT es el system call 8
-    mov esp, 1   ;buscar otro registro
+    mov edx, 1   ;buscar otro registro
     push esp
     call generadorRandoms ;genera el numero
-    fst ;direccion de memoria
+    fst qword[valor];direccion de memoria
+    mov edx, len
+    mov ecx, valor
+    mov eax, 4
+
     int 80h          ;interrupcion del sistema
     jmp _posArchivo
 	
     _sensorTemperatura:
     mov ebx, nom2    ;nombre del archivo
     mov eax, 8       ;SYS_CREAT es el system call 8
-    mov esp, 2
+    mov edx, 2
     push esp
-    call generadorRandoms    
+    call generadorRandoms
+    fst qword[valor]    
     int 80h          ;interrupcion del sistema
     jmp _posArchivo
 
     _sensorElectro:
     mov ebx, nom3    ;nombre del archivo
     mov eax, 8       ;SYS_CREAT es el system call 8
-    mov esp, 3
-    push esp
-    
+    mov edx, 3
+    call generadorRandoms
+    fst qword[valor]
+    push esp   
     int 80h          ;interrupcion del sistema
     jmp _posArchivo
 
     _sensorTasaRespiratoria:
     mov ebx, nom4    ;nombre del archivo
     mov eax, 8       ;SYS_CREAT es el system call 8
-    mov esp, 4
-  
+    mov edx, 4
+    call generadorRandoms
+    fst qword[valor]
     int 80h          ;interrupcion del sistema
     jmp _posArchivo
 
     _sensorSaturacionOxigeno:
     mov ebx, nom5    ;nombre del archivo
     mov eax, 8       ;SYS_CREAT es el system call 8
- 
+    
 
     int 80h          ;interrupcion del sistema
 
