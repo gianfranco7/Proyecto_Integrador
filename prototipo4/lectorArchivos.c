5ce3a518a656c6d6d91404e2f4943d49c85fb817
Gianfranco <56212332+gianfranco7@users.noreply.github.com>
2020-07-11 22:06:13 -0600

    Add files via upload

---
+++ prototipo4/lectorArchivos.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+extern float * lector(FILE *ptr);
+int main(){
+	//leer linea 1
+	FILE* ptr = fopen("sensorPulso.txt","r");
+	char nombreSensor[50];				
+	fgets(nombreSensor, 50, ptr);
+	printf("%s", nombreSensor);
+
+	//lee linea 2 
+	char unidades[50];
+    	fgets(unidades, 50, ptr);
+	printf("%s", unidades);
+	fclose(ptr);
+
+	/*
+	float lecturas[300];
+	for(int i = 0; i < 300; i++){
+		fscanf(ptr, "%f", lecturas);
+		printf("%f ", lecturas[i]);
+	}
+	printf("\n");
+	*/
+	//float lecturas[300];
+	lector(ptr);
+
+	return 0;
+}
