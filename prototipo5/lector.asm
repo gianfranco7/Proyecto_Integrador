01d732298c8036abf271239f8caed3a565f06c12
Gianfranco <56212332+gianfranco7@users.noreply.github.com>
2020-07-13 23:41:05 -0600

    Add files via upload

--- prototipo5/lector.asm
+++ prototipo5/lector.asm
@@ -1,13 +1,27 @@
 SECTION .data
 
 nombreArchivo db 'sensorPulso.txt', 00h
+delimitador db '\0', 00h
+
+;Sensor de pulso       16 con el cambio de linea
+;bpm                   4 con el cambio de linea
+
 
 SECTION .bss
 
 fileDescriptor RESB 4		;para guardar el file descriptor de eax
+nombre RESB 50			
+unit RESB 5
 
-global lector
-lector:
+bufferHora RESB 1
+bufferMin RESB 1
+bufferSeg RESB 1
+bufferLect RESB 4
+
+SECTION .text
+
+global lectorPulso
+lectorPulso:
 
     ;prologo
     push ebp
@@ -17,6 +31,7 @@ lector:
     push ebx
     push ecx
     push edx
+    push esi
 
     ;cuerpo
 	
@@ -25,21 +40,93 @@ lector:
     mov ebx, nombreArchivo	;abrir el archivo con ese nombre
     mov ecx, 0			;read only access
     mov edx, 0o777		;darle permisos
-    ;lo anterior retorna un file descriptor en eax, que sera usado en la lectura
+    int 0x80
 
-    mov fileDescriptor, [eax]	;sacar el file descriptor de eax porque este registro sera utilizado despues
-	
-    ;2.leer del archivo
-    mov eax, 3			;SYS_READ
-    mov ebx, [fileDescriptor] 	;file descriptor en ebx
-    mov ecx, *lecturas   	;puntero al input buffer
-    mov edx, 4			;para pruebas iniciales, 1200 bytes, para el producto finalizado, 2100 bytes
+    mov [fileDescriptor], eax	;sacar el file descriptor de eax porque este registro sera utilizado despues
+      
+    mov eax, 19			;SYS_SEEK
+    mov ebx, [fileDescriptor]	
+    mov ecx, 20			;CANTIDAD DE BYTES A SKIP
+    mov edx, 1			;APARTIR DE DONDE
+    int 0x80
+    
+    mov eax, 3			;SYS READ
+    mov ebx, [fileDescriptor]
+    mov ecx, bufferHora		;output buffer
+    mov edx, 1			;size
+    int 0x80
+   
+    mov eax, 3                  ;SYS READ
+    mov ebx, [fileDescriptor]
+    mov ecx, bufferMin          ;output buffer
+    mov edx, 1                  ;size
+    int 0x80
 
-    ;3.cerrar el archivo
-    mov eax, 6			;SYS_CLOSE
-    mov ebx, [fileDescrpitor]	;mover file descriptor a ebx para que sepa que file cerrar
+    mov eax, 3                  ;SYS READ
+    mov ebx, [fileDescriptor]
+    mov ecx, bufferSeg          ;output buffer
+    mov edx, 1                  ;size
+    int 0x80
+
+    mov eax, 3                  ;SYS READ
+    mov ebx, [fileDescriptor]
+    mov ecx, bufferLect         ;output buffer
+    mov edx, 4                  ;size
+    int 0x80
+
+
+	;IMPRIMIR
+
+    
+    mov esi, [bufferHora]       ;mover a esi la hora
+
+    atoi:
+    xor ebx, ebx
+    mov bl, [esi+ecx]
+    cmp bl, 48    ;48 = '0'
+    jl finAtoI
+    cmp bl, 57    ;57 = '9'
+    jg finAtoI
+
+    sub bl, 48
+    add eax, ebx
+    mov ebx, 10
+    mul ebx
+    inc ecx
+    jmp atoi
+
+    finAtoI:
+    mov ebx, 10
+    div ebx
+    
+    mov [bufferHora], ebx
+
+    mov eax, 4			;SYS OUT
+    mov ebx, 1			;ni idea
+    mov ecx, bufferHora		;output buffer
+    mov edx, 1			;size
+    int 0x80
+
+    mov eax, 4                  ;SYS OUT
+    mov ebx, 1                  ;ni idea
+    mov ecx, bufferMin         ;output buffer
+    mov edx, 1                  ;size
+    int 0x80
+
+    mov eax, 4                  ;SYS OUT
+    mov ebx, 1                  ;ni idea
+    mov ecx, bufferSeg         ;output buffer
+    mov edx, 1                  ;size
+    int 0x80
+
+    mov eax, 4                  ;SYS OUT
+    mov ebx, 1                  ;ni idea
+    mov ecx, bufferLect         ;output buffer
+    mov edx, 4                  ;size
+    int 0x80
 
     ;epilogo
+    pop esi
     pop edx
     pop ecx
     pop ebx
