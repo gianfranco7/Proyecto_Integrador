53aa58337f94da5e48754d986596360114ef327f
KevinGVR <39310978+KevinGVR@users.noreply.github.com>
2020-07-13 21:36:41 -0600

    Graficador beta
    Grafica manualmente segundo a segundo y ya muestra los ultimos 30 segundos del sensor 1

--- src/Graficador/pantalla.cpp
+++ src/Graficador/pantalla.cpp
@@ -18,64 +18,58 @@ pantalla::pantalla(QWidget *parent) :
     QVector<double> S1x(500), S1y(500);
     for(int i=0;i<300;++i){
         S1x[i]=i;
-        S1y[i+200]=i;
+        S1y[i]=i*2;
     }
     QVector<double> S2x(500), S2y(500);
     for(int i=0;i<500;++i){
-
+        S2x[i]=i;
+        S2y[i]=i*i;
     }
     QVector<double> S3x(500), S3y(500);
     for(int i=0;i<500;++i){
-
+        S3x[i]=i;
+        S3y[i]=i*i*i/2;
     }
     QVector<double> S4x(500), S4y(500);
     for(int i=0;i<300;++i){
-
+        S4x[i]=i;
+        S4y[i]=i;
     }
     QVector<double> S5x(500), S5y(500);
     for(int i=0;i<300;++i){
-
+        S5x[i]=i;
+        S5y[i]=i*5;
     }
 
     //Sensor1
     ui->G1->addGraph();
     ui->G1->graph(0)->setData(S1x,S1y);
-    ui->G1->xAxis->setLabel("x");
-    ui->G1->yAxis->setLabel("y");
     ui->G1->xAxis->setRange(0,300);
-    ui->G1->yAxis->setRange(0,500);
+    ui->G1->yAxis->setRange(0,300);
     ui->G1->replot();
     //Sensor2
     ui->G2->addGraph();
     ui->G2->graph(0)->setData(S2x,S2y);
-    ui->G2->xAxis->setLabel("x");
-    ui->G2->yAxis->setLabel("y");
     ui->G2->xAxis->setRange(0,300);
-    ui->G2->yAxis->setRange(0,500);
+    ui->G2->yAxis->setRange(0,400);
     ui->G2->replot();
     //Sensor3
     ui->G3->addGraph();
     ui->G3->graph(0)->setData(S3x,S3y);
-    ui->G3->xAxis->setLabel("x");
-    ui->G3->yAxis->setLabel("y");
     ui->G3->xAxis->setRange(0,300);
-    ui->G3->yAxis->setRange(0,500);
+    ui->G3->yAxis->setRange(0,100);
     ui->G3->replot();
     //Sensor4
     ui->G4->addGraph();
     ui->G4->graph(0)->setData(S4x,S4y);
-    ui->G4->xAxis->setLabel("x");
-    ui->G4->yAxis->setLabel("y");
     ui->G4->xAxis->setRange(0,300);
-    ui->G4->yAxis->setRange(0,500);
+    ui->G4->yAxis->setRange(0,100);
     ui->G4->replot();
     //Sensor5
     ui->G5->addGraph();
     ui->G5->graph(0)->setData(S5x,S5y);
-    ui->G5->xAxis->setLabel("x");
-    ui->G5->yAxis->setLabel("y");
     ui->G5->xAxis->setRange(0,300);
-    ui->G5->yAxis->setRange(0,500);
+    ui->G5->yAxis->setRange(0,100);
     ui->G5->replot();
 
 }
@@ -85,7 +79,37 @@ pantalla::~pantalla()
     delete ui;
 }
 
-void pantalla::on_pushButton_clicked(bool checked)
+int l=0;
+void pantalla::on_pushButton_clicked()
 {
-    ui->L3->setText("holi");
+    ui->G1->xAxis->setRange(l-30,l);
+    ui->G1->replot();
+    ui->G1->update();
+}
+
+void pantalla::on_pushButton_2_clicked()
+{
+    ui->G1->xAxis->setRange(0,300);
+    ui->G1->replot();
+    ui->G1->update();
+}
+
+void pantalla::on_pushButton_3_clicked()
+{
+    ui->G1->xAxis->setRange(0,l);
+    ui->G1->replot();
+    ui->G1->update();
+    ui->G2->xAxis->setRange(0,l);
+    ui->G2->replot();
+    ui->G2->update();
+    ui->G3->xAxis->setRange(0,l);
+    ui->G3->replot();
+    ui->G3->update();
+    ui->G4->xAxis->setRange(0,l);
+    ui->G4->replot();
+    ui->G4->update();
+    ui->G5->xAxis->setRange(0,l);
+    ui->G5->replot();
+    ui->G5->update();
+    l++;
 }
--- src/Graficador/pantalla.h
+++ src/Graficador/pantalla.h
@@ -19,7 +19,13 @@ public:
 
 
 private slots:
-    void on_pushButton_clicked(bool checked);
+
+
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
 
 private:
     Ui::pantalla *ui;
