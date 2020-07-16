e20b651e180f7f6fc3fabbc1e4a3ee05b759f9a8
Jose14valerio <56236035+Jose14valerio@users.noreply.github.com>
2020-07-16 16:30:37 -0600

    Add files via upload
    Prueba del maximo, minimo y promedio

--- prototipo5/main.c
+++ prototipo5/main.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
-extern void generadorArchivos(int tipoArchivo);
 
-void main() {
-    generadorArchivos(1);
-   // generadorArchivos(2);
-   // generadorArchivos(3);
-   // generadorArchivos(4);
-   // generadorArchivos(5);
+extern double mayor (double a, double b);
+extern double menor (double a, double b);
+extern double promedio (double a, double b);
+
+int main() {
+    double a, b, c, d;
+
+    printf("un numero: ");
+    scanf("%lf", &a);
+    printf("un numero: ");
+    scanf("%lf", &b);
+    printf("El mayor de %lf y %lf es: %lf\n", a, b, mayor(a, b));
+
+    printf("un numero: ");
+    scanf("%lf", &c);
+    printf("un numero: ");
+    scanf("%lf", &d);
+    printf("El menor de %lf y %lf es: %lf\n", c, d, menor(c,d));
+     
+    printf("El promedio de %lf y %lf es: %lf\n", 100.0, 10.0, promedio(100.0, 10.0));
+    return 0;
 }
